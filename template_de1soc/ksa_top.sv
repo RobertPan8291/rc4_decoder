7b51156b43aa9beda1329235fcc66a54a74d18c1
Robert Pan <rpan0827@gmail.com>
2024-06-10 12:43:26 -0700

    before overhaul

--- template_de1soc/ksa_top.sv
+++ template_de1soc/ksa_top.sv
@@ -26,11 +26,20 @@ module ksa_top(
 	
 	logic finish_generation = 1'b0;
 
-	parameter [3:0] INITIALIZE = 4'b0001,
-						 SHUFFLE = 4'b0010;
+	parameter [3:0] INITIALIZE = 4'b0000,
+						 SHUFFLE = 4'b0010,
+						 WAIT = 4'b0011,
+						 RECORD = 4'b0100,
+						 COMPUTE_J = 4'b0101;
 						 
 		
 	logic [3:0] state = INITIALIZE;
+	
+	logic [7:0] counter_i = 8'd0;
+	
+	logic [7:0] counter_j = 8'd0;
+	
+	logic [7:0] temp_reg;
 	
 	s_memory my_mem(
 		.data(data),
@@ -44,18 +53,23 @@ module ksa_top(
 
 	always_ff @(posedge clk) begin
 		if(state == INITIALIZE) begin
-				wren <= 1'b1; 
-				address <= address + 1;
-				data <= data + 1;
-				rden <= 1'b0;
+			wren <= 1'b1; 
+			address <= address + 1;
+			data <= data + 1;
+			rden <= 1'b0;
 		end 
 		else if(state == SHUFFLE) begin
-				wren <= 1'b0;
-				address <= address + 1;				
-				rden <= 1'b1;
-
-
+			wren <= 1'b0;
+			rden <= 1'b1; 
+			address <= address + 1;
+			counter_i <= counter_i + 1;
 		end
+		else if(state == RECORD) begin
+			temp_reg <= q;
+		end
+		//else if(state == COMPUTE_J) begin
+			//counter_j <= counter_j + temp_reg;
+		//end
 	end
 	
 	always_ff @(posedge clk) begin
@@ -64,10 +78,13 @@ module ksa_top(
 		end else begin
 			case(state)
 				INITIALIZE: begin
-									if(address == 8'd254) begin
+									if(address == 8'd255) begin
 										state <= SHUFFLE;
 									end
 								end
+				SHUFFLE: state <= WAIT;
+				WAIT: state <= RECORD;
+				RECORD: state <= SHUFFLE;
 			endcase
 		end
 	
